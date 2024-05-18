2628ea566c38fb9c7739e4469b8505f3c5055ccb
gurleensingh5376 <gurleensingh5376@gmail.com>
2024-05-18 14:06:35 +0530

    11

--- Backend/server.js
+++ Backend/server.js
@@ -1,87 +1,66 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
-const app = express()
-const bcrypt = require("bcryptjs");
-const saltRounds = 10;
- 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+
+const app =express()
+
+//send data through backend to frontend
+// const card=[
+//   {
+//     para:"this is 1 card"
+//   },
+//   {
+//     para:"this is 2 card"
+//   }, {
+//     para:"this is 3 card"
+//   }, {
+//     para:"this is 4 card"
+//   }, {
+//     para:"this is 5 card"
+//   }, {
+//     para:"this is 6 card"
+//   }, {
+//     para:"this is 7 card"
+//   },
+// ]
+
+
+
+// Coneect to mongodb
+mongoose.connect('mongodb://localhost:27017/test');
+
+// data convert into json
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//cross-origin  
+app.use(function(req, res, next) {   
+res.header("Access-Control-Allow-Origin","*");
+res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+next();
 });
 
 
-mongoose.connect("mongodb://localhost:27017/test").then(() => console.log("Connected!"));
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
 
-app.use(bodyParser.json()); 
-
-const data = new mongoose.Schema({
-  email: String,
-  password: String,
+// Creating Schema using mangoose
+const data= new mongoose.Schema({email:String,password:String
 });
 
-const auth = mongoose.model("auths", data);
+// Creating model using schema
+const auth=mongoose.model('auths',data);
 
-app.post('/api/auths',(req,res)=>{
-  console.log(req.body);
-  const password = req.body.password;
-  const email = req.body.email;
 
-  auth.findOne({email}).then((val)=>{
-  
-   if(val==null)
-   {
-     console.log("No data found")
-     
-     bcrypt.hash(password,saltRounds,function(err,hash){
-       console.log(hash)
-       let sendauth = new auth({email:email,password:hash});
-     sendauth.save().then((val)=>{
-       console.log(val)
-       res.json(val)
-     })
-   });
-     
-   }
-   else{
-     console.log("user already exist")
-   }
- })
-})
-
-app.post('/login',(req,res)=>{
-  const email = req.body.email;
-  console.log(email);
-  auth.findOne({email : email}).then((val) =>{
-
-     if( val != null) {
-     console.log(val);
-     
-     bcrypt.compare(req.body.password,val.password , function(err , result){
-      console.log(result);
-      res.json(result);
-     })
-  }else{
-      res.json("Enter Correct Email")
-      // console.log(val)
-  }
+app.post('/api/auth',(req, res)=>{
+   console.log(req.body)
+  let sendauth = new auth(req.body);
+  sendauth.save().then((val)=>{
+    console.log(val)
+    res.json(val)
   })
-})
+});
 
-app.get('/dash',(req,res)=>{
-  res.json()
-})
 
-app.listen(4000, () => {
-  console.log("listening port 4000");
+app.listen(4000, (res) => {
+  console.log('Listening on port 4000');
 });
